fa0aabdf4fb932ea0eadc40d992f72e785a0abe5
urlv <urlv>
2018-10-18 23:23:48 +0300

    Init

---
+++ app.js
@@ -0,0 +1,17 @@
+const search = require('./lib/search')
+
+function main(args){
+    if (args.length !== 5){
+        console.error('You need to enter 3 argumants:\n' +
+                      '1. file extension\n' +
+                      '2. text to search\n' +
+                      '3. starting path')
+    }else{
+        search.start(args[2], args[3], args[4])
+        process.on('beforeExit', () => {
+            if(search.occurrences() === 0) console.log('No file was found')
+        })
+    }
+}
+
+main(process.argv)
